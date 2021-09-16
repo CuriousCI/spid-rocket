62e37724289ce642577b4d46dc01bcb3a77520b4
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-09-16 19:55:46 +0200

    added identity providers and service providers

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 mod attributes;
+mod idp;
+mod sp;
 
 pub use self::attributes::Attributes;
+pub use self::idp::IdentityProvider;
+pub use self::sp::ServiceProvider;
 
 pub fn test() -> &'static str {
 	"Hello, SPID!"
